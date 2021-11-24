21fcf37b2fd9b5bde70218cc8453f7b74bff8777
Nicholas Alayra <nalayra@hotmail.com>
2021-11-24 14:49:48 -0800

    commit tree 6612412443f67c226df9ab1bc7531beba673d39d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function findTheW(record) {
+    return (record.result === "W")
+}
 
+function superbowlWin(record) {
+    let element = record.find(findTheW);
+
+    if (element !== undefined) {
+        return element.year;
+    }
+    else {
+        return element;
+    }
+}
